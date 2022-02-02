6342729f938b6c3b330a720c9666898fead0feb3
大漠穷秋 <damoqiongqiu@126.com>
2022-02-02 16:07:00 +0800

    update-comment

--- src/graphic/shape/ICERect.ts
+++ src/graphic/shape/ICERect.ts
@@ -13,6 +13,7 @@ class ICERect extends ICEDotPath {
    * 计算路径上的关键点:
    * - 默认的坐标原点是 (0,0) 位置。
    * - 这些点没有经过 transform 矩阵变换。
+   * this.calcOriginalDimension() 会依赖此方法，在计算尺寸时还没有确定原点坐标，所以 calcDots() 方法内部不能依赖原点坐标，只能基于组件本地坐标系的左上角 (0,0) 点进行计算。
    * @returns
    */
   protected calcDots(): Array<DOMPoint> {
