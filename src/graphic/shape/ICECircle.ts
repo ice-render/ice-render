8573586616ebfd2eeff1cdaefe0c6479be0dc682
大漠穷秋 <damoqiongqiu@126.com>
2022-02-06 19:21:13 +0800

    refactor-ICECircle

--- src/graphic/shape/ICECircle.ts
+++ src/graphic/shape/ICECircle.ts
@@ -1,34 +1,17 @@
-import ICEPath from '../ICEPath';
+import ICEEllipse from './ICEEllipse';
 
 /**
- * FIXME:采用椭圆绘制方法，正圆形作为椭圆的特殊情况处理。
+ * 采用椭圆绘制方法，正圆形作为椭圆的特殊情况处理。
  * @class ICECircle
  * @author 大漠穷秋<damoqiongqiu@126.com>
  */
-class ICECircle extends ICEPath {
+class ICECircle extends ICEEllipse {
   constructor(props: any = {}) {
-    super({ radius: 10, ...props });
-    this.props.width = this.props.radius * 2;
-    this.props.height = this.props.radius * 2;
-    this.state.width = this.props.radius * 2;
-    this.state.height = this.props.radius * 2;
-  }
+    let param = { radius: 10, ...props };
+    param.radiusX = param.radius;
+    param.radiusY = param.radius;
 
-  /**
-   * 所有坐标点的坐标都是相对于父层组件，而不是全局坐标。
-   * @returns
-   */
-  protected createPathObject(): Path2D {
-    this.path2D = new Path2D();
-    this.path2D.arc(
-      this.state.radius - this.state.localOrigin.x,
-      this.state.radius - this.state.localOrigin.y,
-      this.state.radius,
-      0,
-      Math.PI * 2
-    );
-    this.path2D.closePath();
-    return this.path2D;
+    super(param);
   }
 }
 export default ICECircle;
