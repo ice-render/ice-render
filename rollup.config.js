0073f5cfaa133ed62454a1d4935a47e4b0cfc257
大漠穷秋 <damoqiongqiu@126.com>
2022-03-10 15:08:15 +0800

    fix uuid, refactor code

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,9 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
+
 const env = process.env.NODE_ENV;
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-//TODO:make lodash external library.
 const CommonPlugins = [
   json(),
   nodeResolve({ extensions }),
@@ -17,6 +17,8 @@ const CommonPlugins = [
   }),
   env === 'production' && terser(),
 ].filter(Boolean);
+const external = [...Object.keys(pkg.devDependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+const globals = {};
 
 /**
  * support config Intellisense
@@ -25,41 +27,45 @@ const CommonPlugins = [
 const configs = [
   {
     input: 'src/index.ts',
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-      },
-      {
-        file: pkg.module,
-        format: 'esm',
-      },
-    ],
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      globals: { ...globals },
+    },
     plugins: CommonPlugins,
+    external: [...external],
   },
   {
     input: 'src/index.ts',
-    output: [
-      {
-        name: 'boilerplate',
-        file: pkg.browser,
-        format: 'umd',
-      },
-    ],
-
+    output: {
+      file: pkg.module,
+      format: 'esm',
+      globals: { ...globals },
+    },
     plugins: CommonPlugins,
+    external: [...external],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'boilerplate',
+      file: pkg.browser,
+      format: 'umd',
+      globals: { ...globals },
+    },
+    plugins: CommonPlugins,
+    external: [...external],
   },
   {
     input: 'tests/index.ts',
-    output: [
-      {
-        name: 'boilerplate',
-        file: 'tests/index.umd.js',
-        format: 'umd',
-      },
-    ],
+    output: {
+      name: 'boilerplate',
+      file: 'tests/index.umd.js',
+      format: 'umd',
+      globals: { ...globals },
+    },
     plugins: CommonPlugins,
+    external: [...external],
   },
 ];
 export default configs;
