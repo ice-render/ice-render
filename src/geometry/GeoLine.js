e0a323b2c779513d7b474a962f2bd327d34a14af
大漠穷秋 <damoqiongqiu@126.com>
2022-03-22 11:24:48 +0800

    refactor-GeoLine

--- src/geometry/GeoLine.js
+++ src/geometry/GeoLine.js
@@ -22,37 +22,14 @@ export default class GeoLine {
     this.endPoint = endPoint;
   }
 
-  /**
-   * @method constants
-   * Tests to see if a point belongs to this line (not as infinite line but more like a segment)
-   * Algorithm: Compute line's equation and see if (x, y) verifies it.
-   *
-   * 测试某个点是否位于直线上（这里不是数学意义上的无线延长直线，而是线段）。
-   * 算法：计算斜率，看(x,y)点是否位于线段上。
-   *
-   * @see http://www.jeffreythompson.org/collision-detection/line-point.php
-   * @param {Number} x - the X coordinates
-   * @param {Number} y - the Y coordinates
-   */
   contains(x, y) {
-    // if the point is inside rectangle bounds of the segment
-    if (
-      Math.min(this.startPoint.x, this.endPoint.x) <= x &&
-      x <= Math.max(this.startPoint.x, this.endPoint.x) &&
-      Math.min(this.startPoint.y, this.endPoint.y) <= y &&
-      y <= Math.max(this.startPoint.y, this.endPoint.y)
-    ) {
-      // check for vertical line
-      if (this.startPoint.x == this.endPoint.x) {
-        return x == this.startPoint.x;
-      } else {
-        // usual (not vertical) line can be represented as y = a * x + b
-        let a = (this.endPoint.y - this.startPoint.y) / (this.endPoint.x - this.startPoint.x);
-        let b = this.startPoint.y - a * this.startPoint.x;
-        return y == a * x + b;
-      }
-    } else {
-      return false;
+    let delta = 3;
+    let lineLength = Math.hypot(this.endPoint.x - this.startPoint.x, this.endPoint.y - this.startPoint.y);
+    let len1 = Math.hypot(x - this.startPoint.x, y - this.startPoint.y);
+    let len2 = Math.hypot(x - this.endPoint.x, y - this.endPoint.y);
+    if (len1 + len2 >= lineLength - delta && len1 + len2 <= lineLength + delta) {
+      return true;
     }
+    return false;
   }
 }
