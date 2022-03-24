25777f3f7778fe14b53aa6bb3acf726be65ccbf5
大漠穷秋 <damoqiongqiu@126.com>
2022-03-24 13:29:53 +0800

    refactor-uuid.js

--- src/util/uuid.js
+++ src/util/uuid.js
@@ -1,12 +1,18 @@
 /**
  * @see https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
  */
+import root from '../cross-platform/root';
+
 export function uuid() {
-  var dt = Date.now();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = (dt + Math.random() * 16) % 16 | 0;
-    dt = Math.floor(dt / 16);
-    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
-  });
-  return uuid;
+  if (root.crypto) {
+    return root.crypto.randomUUID();
+  } else {
+    var dt = Date.now();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+    });
+    return uuid;
+  }
 }
