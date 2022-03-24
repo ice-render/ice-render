46fa032faeda5d4ea2895b4dc25cb4573cd6ffaf
大漠穷秋 <damoqiongqiu@126.com>
2022-03-24 13:29:53 +0800

    refactor-uuid.js

--- tests/index.umd.js
+++ tests/index.umd.js
@@ -3554,13 +3554,17 @@
    * @see https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
    */
   function uuid() {
-    var dt = Date.now();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = (dt + Math.random() * 16) % 16 | 0;
-      dt = Math.floor(dt / 16);
-      return (c == 'x' ? r : r & 0x3 | 0x8).toString(16);
-    });
-    return uuid;
+    if (root$2.crypto) {
+      return root$2.crypto.randomUUID();
+    } else {
+      var dt = Date.now();
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : r & 0x3 | 0x8).toString(16);
+      });
+      return uuid;
+    }
   }
 
   /**
