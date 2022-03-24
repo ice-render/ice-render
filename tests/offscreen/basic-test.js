afdf71cf408277a29c4f40cef1b3594db648ec2a
大漠穷秋 <damoqiongqiu@126.com>
2022-03-24 13:13:37 +0800

    updat-tests

--- tests/offscreen/basic-test.js
+++ tests/offscreen/basic-test.js
@@ -5,11 +5,13 @@ ctx.strokeStyle = '#ff0000';
 ctx.fillStyle = '#0000ff';
 
 let startTime = Date.now();
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 2000; i++) {
+  ctx.setTransform(Math.random() * 10, 0, 0, Math.random() * 10, 0, 0);
   ctx.rect(Math.random() * 1024, Math.random() * 768, 50, 50);
 }
 ctx.stroke();
 ctx.fill();
+ctx.setTransform(1, 0, 0, 1, 0, 0);
 
 let endTime = Date.now();
 console.log(`Time> ${endTime - startTime} ms.`);
