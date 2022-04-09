b6d67b08079c14894b2f5ea76fbde14f3a37de19
大漠穷秋 <damoqiongqiu@126.com>
2022-04-09 22:03:25 +0800

    update comments

--- src/control-panel/transform-controls/ResizeControl.ts
+++ src/control-panel/transform-controls/ResizeControl.ts
@@ -17,13 +17,17 @@ import ICERect from '../../graphic/shape/ICERect';
  * - 调整尺寸的操作手柄不能独立存在，只能依附在某个宿主对象上。
  * - 此手柄只能调整尺寸，不能实现翻转
  *
- * TODO: 补全 props 配置项
- * {
- *   direction: 改变的方向，共有3个可选的值： x/y/both
- *   position: 手柄在变换矩形4个边上的位置，共有8个：lt/t/rt/r/rb/b/lb/l
- * }
  */
 export default class ResizeControl extends ICERect {
+  /**
+   * props:
+   * {
+   *   direction: 改变的方向，共有3个可选的值： x/y/both
+   *   position: 手柄在变换矩形4个边上的位置，共有8个：lt/t/rt/r/rb/b/lb/l
+   *   quadrant: 当前所在的象限
+   * }
+   * @param props
+   */
   constructor(props) {
     super({ position: 'l', direction: 'x', quadrant: 1, ...props, linkable: false });
   }
@@ -34,7 +38,6 @@ export default class ResizeControl extends ICERect {
   }
 
   /**
-   * 围绕几何中心点调整宽高。
    * @param evt
    */
   private resizeEvtHandler(evt) {
@@ -119,6 +122,7 @@ export default class ResizeControl extends ICERect {
 
   /**
    * @overwrite
+   * @method moveGlobalPosition
    * ResizeControl 不能自由移动自己的位置，自能在X轴、Y轴，以及矩形的2条对角线上移动位置。
    *
    * 在全局空间(canvas)中移动指定的位移。
